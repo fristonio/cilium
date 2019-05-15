08ce9fcb880fb6305a7d7f2e421bc69b16071df0
Nirmoy Das <ndas@suse.de>
2019-05-15 13:04:29 +0200

    cli: Restore cilium cleanup behaviour
    when no flags(--all-state|--bpf-state) are specified fallback to
    --all-state to keep the previous cilium cleanup behaviour
    
    Fixes: 6145cb56c89086 ("k8s: Merge initContainer cleanup with cilium cleanup")
    
    Signed-off-by: Nirmoy Das <ndas@suse.de>

--- contrib/packaging/docker/init-container.sh
+++ contrib/packaging/docker/init-container.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+if [ "${CLEAN_CILIUM_STATE}" = "true" ] \
+   || [ "${CLEAN_CILIUM_BPF_STATE}" = "true" ]; then
 cilium cleanup -f
+fi
 
 if [ "${CILIUM_WAIT_BPF_MOUNT}" = "true" ]; then
 	until mount | grep bpf; do echo "BPF filesystem is not mounted yet"; sleep 1; done
